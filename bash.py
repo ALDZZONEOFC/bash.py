b20520bcac40242e11019632189b0eaeaed0ce4a
ALDZZONEOFC <160108714+ALDZZONEOFC@users.noreply.github.com>
2024-02-15 14:37:55 +0700

    Create bash.py

---
+++ bash.py
@@ -0,0 +1,5 @@
+U͞N͞T͞U͞K͞ M͞E͞M͞P͞E͞R͞M͞U͞D͞A͞H͞ H͞I͞D͞U͞P͞ A͞N͞D͞A͞
+ ʝαиgαи ℓυρα ѕυвѕ¢яιвє ¢нαииєℓ 🎋 YRALDO🎋
+ Creator: YRALDO
+ https://github.com/SAMTROOFFICIAL3300/SCRIPTENCBY
+""".format(D,W,D,W,D,W,Y,W,D,W,D,W,D,W,D,W,D,Y,D,W,D,Y,D,G,W,G,D,G,W,G,Y,D,Y,D,Y,D,Y,D,Y)
